8656e4f8445aa5cf2c4eb9116601c1041e31ff3a
Matt Day <mattsday@gmail.com>
2016-06-21 08:21:02 +0100

    Added docker image

---
+++ docker-image/voter.py
@@ -0,0 +1,19 @@
+import urllib2
+import thread
+import time
+import sys
+
+my_hero = sys.argv[1]
+base_url = "http://myhero-web.metacloud.mrsmiggins.net/results?hero="
+
+def vote (name):
+	url = base_url + name
+	while (True):
+		urllib2.urlopen(url)
+
+for i in range(0, 4):
+	thread.start_new_thread(vote, (urllib2.quote(my_hero),))
+
+while (True):
+	time.sleep(100)
+
