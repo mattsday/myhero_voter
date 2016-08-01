00f9826e7416aae50dc42e548f465fa56b7822a1
Matt Day <mattsday@gmail.com>
2016-08-01 06:04:19 +0100

    Fixed URL escape

--- docker-image/voter.py
+++ docker-image/voter.py
@@ -2,7 +2,8 @@ import urllib2, os, thread, time, sys
 
 try:
 	# Construct URL:
-	vote_url = os.environ['MY_HERO_URL'] + "/results?hero=" + os.environ['MY_HERO']
+	vote_url = os.environ['MY_HERO_URL'] + "/results?hero=" + \
+		urllib2.quote(os.environ['MY_HERO'])
 	# Thread count
 	vote_threads = int(os.environ['MY_HERO_THREADS'])
 except KeyError:
@@ -16,8 +17,8 @@ def vote (url):
 	while (True):
 		urllib2.urlopen(url)
 
-for i in range(0, (vote_threads - 1)):
-	thread.start_new_thread(vote, (urllib2.quote(vote_url),))
+for i in range(0, (vote_threads)):
+	thread.start_new_thread(vote, (vote_url,))
 
 while (True):
 	time.sleep(100)
